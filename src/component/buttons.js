af593711effe77a5362ea5c1ac970c6a59dd7452
Petra Boldi <boldi.petra@gmail.com>
2021-04-15 18:14:02 +0200

    buttons file created

---
+++ src/component/buttons.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Button extends React.Component{
+    constructor(props){
+        super(props)
+    }
+ render(){
+     return <button></button>
+ }
+}
+export default Heading
